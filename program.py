a7ec88fc3b770496fb487cb4938171d54fb776cf
Your Name <murataliev07@gmail.com>
2024-11-01 19:19:55 +0600

    изменение 2

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 
-for item in range(10):
-    for i in range(10):
-        print(item, " ", i)
-
+for item in range(1, 10):
+    for i in range(1, 10):
+        if i % item == 0:
+            print(item, " ", i)
